d84c2196fde65100ccd7b399cea9b65414628b4e
CarlosGuedea <guedea.fis@gmail.com>
2022-07-10 09:37:54 -0500

    scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 function iniciarApp() {
   crearGaleria();
+  scrollNav();
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".producto");
+  enlaces.forEach((enlace) => {
+    enlaces.addEventListener("scroll", function (e) {
+      const seccion = document.querySelector(e.target.attributes.href.value);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
